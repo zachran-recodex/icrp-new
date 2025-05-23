86d5e62f26eafc8a51e0cd30eb3d9323b43f2c4b
Zachran Razendra <zachranraze@recodex.id>
2025-05-24 01:01:30 +0700

    feat(layouts): add Font Awesome and update color scheme
    - Include Font Awesome CSS for icon support in the main layout
    - Update the auth layout to redirect to 'beranda' instead of 'home'
    - Extend the color scheme in app.css with primary, secondary, and tertiary variants

--- resources/views/components/layouts/main.blade.php
+++ resources/views/components/layouts/main.blade.php
@@ -16,6 +16,10 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
+              integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
+              crossorigin="anonymous" referrerpolicy="no-referrer" />
+
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
